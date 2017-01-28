df8b68b39524f78aa666cd693b8197a0f28e3ed8
Martin <mjakal@gmail.com>
2017-01-28 21:01:26 +0100

    fixed user controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     # debugger
 
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
 
     if @article.save
       flash[:success] = "Article was successfully created"
