e9869d5ba359ae911f8927aab0f788e09f215f49
Martin <mjakal@gmail.com>
2017-02-08 20:06:22 +0100

    minor style fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :show]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -9,7 +10,7 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-   def create
+  def create
     @category = Category.new(category_params)
 
     if @category.save
@@ -21,23 +22,19 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
+    
   end
 
   def update
-    @category = Category.find(params[:id])
-
     if @category.update(category_params)
       flash[:success] = "Category was successfully updated"
       redirect_to category_path(@category)
     else
       render "edit"
     end
-
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -45,6 +42,10 @@ class CategoriesController < ApplicationController
   
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
     end
 
     def require_admin
